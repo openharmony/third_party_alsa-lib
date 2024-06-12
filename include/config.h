52d192defea6a7ab67caffb1b4f895f3bec51084
whoselittlelion <humin16@huawei.com>
2024-06-12 16:17:29 +0800

    fix: fix opensource notice
    Signed-off-by: whoselittlelion <humin16@huawei.com>

--- include/config.h
+++ include/config.h
@@ -1,22 +1,5 @@
-/*
- *  ALSA lib header file include/config.h
- *  Copyright (c) 2022 Huawei Device Co., Ltd.
- *
- *   This library is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Lesser General Public License as
- *   published by the Free Software Foundation; either version 2.1 of
- *   the License, or (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU Lesser General Public License for more details.
- *
- *   You should have received a copy of the GNU Lesser General Public
- *   License along with this library; if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- */
+/* include/config.h.  Generated from config.h.in by configure.  */
+/* include/config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Directory with aload* device files */
 #define ALOAD_DEVICE_DIRECTORY "/dev/"
