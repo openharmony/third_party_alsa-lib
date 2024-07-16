9f3dce3d8860cb74fa4c411649f81b9e7be518d1
许学海 <xuxuehai2@huawei.com>
2024-07-16 11:30:00 +0000

    update include/version.h.
    Signed-off-by: 许学海 <xuxuehai2@huawei.com>

--- include/version.h
+++ include/version.h
@@ -1,5 +1,21 @@
 /*
- *  version.h
+ *  ALSA lib header file include/version.h
+ *  Copyright (c) 2022 Huawei Device Co., Ltd.
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation; either version 2.1 of
+ *   the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
  */
 
 #define SND_LIB_MAJOR		1 /**< major number of library version */
